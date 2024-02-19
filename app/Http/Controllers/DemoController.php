6fad794e442f6967d69a3daa832f2ac1b3e3566a
Md Rasel Rana <raselges127@gmail.com>
2024-02-20 00:04:46 +0600

    added

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -9,4 +9,33 @@ class DemoController extends Controller
     public function Homepage(){
         return view("pages.hero");
     }
+    public function FindJob(){
+        return view("pages.Find-jobs");
+    }
+    public function LogInpage(){
+        return view("pages.login");
+    }
+    public function register(){
+        return view("pages.register");
+    }
+    
+    public function jobjost(){
+        return view("pages.job-post");
+    }
+    public function accountpage(){
+        return view("pages.account");
+    }
+    public function myjobpage(){
+        return view("pages.my-job");
+    }
+    public function appliedjob(){
+        return view("pages.job-applied");
+    }
+    public function savejobpage(){
+        return view("pages.job-applied");
+    }
+
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,15 @@
 use App\Http\Controllers\DemoController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
+
 
 Route::get('/',[DemoController::class,'Homepage']);
+Route::get('/find-job',[DemoController::class,'FindJob']);
+Route::get('/login',[DemoController::class,'LogInpage']);
+Route::get('/userRegistration',[DemoController::class,'register']);
+Route::get('/job-post',[DemoController::class,'jobjost']);
+Route::get('/account',[DemoController::class,'accountpage']);
+Route::get('/my-job',[DemoController::class,'myjobpage']);
+Route::get('/applied-job',[DemoController::class,'appliedjob']);
+Route::get('/save-job',[DemoController::class,'savejobpage']);
